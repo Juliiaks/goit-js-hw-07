643b1fffd9042efd0b67b3274be86fa05d97329f
Juliiaks <juliakharetonchuk@gmail.com>
2024-03-03 00:06:43 +0200

    add css task 1

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+const list = document.querySelectorAll(".item");
+console.log(`Number of categories: ${list.length}`);
+
+const elemHeader = document.querySelector("h2");
+console.log(`Category: ${elemHeader.textContent}`);
+
+
+
+
+// let total = 0;
+// list.forEach(function (item) {
+//     total += item
+//     return total;
+// });
+
+// const category = document.querySelector(".item");
+// console.log(category.textContent(h2));
