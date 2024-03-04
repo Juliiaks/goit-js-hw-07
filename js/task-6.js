e2282770687030e5f4180c5753a716976bfa7d02
Juliiaks <juliakharetonchuk@gmail.com>
2024-03-05 01:32:30 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputDiv = document.querySelector("#controls");
+
+const input = document.querySelector("input");
+
+const btnCreate = document.querySelector("button[data-create]");
+
+const btnDestroy = document.querySelector("button[data-destroy]");
+
+const divBoxes = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", handleClick);
+function handleClick() {
+  const boxCount = input.value;
+    if (boxCount >= 1 && boxCount <= 100) {
+      createBoxes(boxCount);
+      input.value = " ";
+      input.reset();
+    }
+}
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++){
+    const newBoxes = document.createElement("div");
+      newBoxes.style.width = "30px";
+      newBoxes.style.height = "30px";
+      newBoxes.style.backgroundColor = getRandomHexColor();
+      divBoxes.append(newBoxes)
+      
+  }
+  
+}
+
+btnDestroy.addEventListener("click",  => divBoxes.remove(newBoxes))
+
+function removeBoxes() {
+  
+}
