9c6c745dcbd83d5b036da91492d7401d1d42e022
Juliiaks <juliakharetonchuk@gmail.com>
2024-03-04 12:55:27 +0200

    task-3

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("#name-input");
+
+const span = document.querySelector("#name-output");
+
+
+input.addEventListener("input", (event) => {
+    if (event.currentTarget.value === "" || event.currentTarget.value.trim() === ""){
+        span.textContent = "Anonymous"
+    } else {
+        span.textContent = event.currentTarget.value.trim();
+    }
+});
+
+
