91077bac3aa3399d9e6a6d5086fba4ee0e902c8c
Juliiaks <juliakharetonchuk@gmail.com>
2024-03-04 22:02:26 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,23 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+console.log(getRandomHexColor());
+const button = document.querySelector(".change-color");
+let span = document.querySelector(".color")
+
+
+button.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+});
+
+
+// Варіант 2
+
+// button.addEventListener("click", changeColor);
+//  function changeColor() {
+//   const color = getRandomHexColor();
+//   const body = document.body;
+//   body.style.backgroundColor = color;
+//   span.textContent = color;
+// }
